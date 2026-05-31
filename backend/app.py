2d3acb9b43f0b6a3fdc7081bbd49074897e4f711
NickKaralis <s4094252@student.rmit.edu.au>
2026-05-31 19:41:40 +1000

    commit

--- backend/app.py
+++ backend/app.py
@@ -23,4 +23,4 @@ register_routes(app, db)
 stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
